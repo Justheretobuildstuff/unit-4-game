fccce92b3e7e1bfea29b9ba7863c2e8d75a8444e
Josh Daniels <joshdaniels@Joshs-iMac.local>
2018-05-29 11:15:57 -0700

    changing to jQuery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,29 +7,31 @@ var bluebutton = 0;
 var yellowbutton = 0;
 var greenbuttton = 0;
 var myRandomNumber = 0;
+var anotherRandomNumber = 0;
 
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+var myRandomNumber = Math.floor(Math.random() * 2);
+console.log(myRandomNumber);
 
 var red = document.getElementById("red");
 var blue = document.getElementById("blue");
 var yellow = document.getElementById("yellow");
 var green = document.getElementById("green");
 
+var anotherRandomNumber = document.getElementById("button-1");
+
+
+
+
+/*
+$(document).ready(function () {
+$('#losses').html(losses);
+$('#wins').html(wins);
+$('#ourTotal').html(ourTotal);
 $('#myRandomNumber').text(getRandomInt(19, 121));
 
-$("ourTotal").click(getRandomInt(1, 13))
-    $("ourTotal").append()
-
-$(document).ready(function () {
-    
-
-$('#ourTotal').text(getRandomInt(1, 13))
+$('#ourTotal').on('click', 'button-1', getRandomInt(1, 13));
 
 });
-
+*/
 
