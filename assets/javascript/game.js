bbcd74210b3feeb7e7265c5400be779edc8e59d1
Josh Daniels <joshdaniels@Joshs-iMac.local>
2018-05-29 13:43:31 -0700

    game works, just won't reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,48 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-// Creating my variables
-var wins = 0;
-var losses = 0;
-var ourTotal = 0;
-var redButton = 0;
-var blueButton = 0;
-var yellowButton = 0;
-var greenbButton = 0;
+    // Creating my variables
+    var wins = 0;
+    var losses = 0;
+    var redButton = 0;
+    var blueButton = 0;
+    var yellowButton = 0;
+    var greenbButton = 0;
 
+    $("#wins").html(wins);
+    $("#losses").html(losses);
 
-var RandomNumber = Math.floor(Math.random() * ((120-19)+1) + 19);
-var redButton = Math.floor(Math.random() * ((12-1)+1) + 1);
-var blueButton = Math.floor(Math.random() * ((12-1)+1) + 1);
-var yellowButton = Math.floor(Math.random() * ((12-1)+1) + 1);
-var greenButton = Math.floor(Math.random() * ((12-1)+1) + 1);
+    var RandomNumber = 0;
+    var redButton = 0;
+    var blueButton = 0;
+    var yellowButton = 0;
+    var greenButton = 0;
+    var ourTotal = 0;
 
-    function startOver(result) {
-    $("#ourTotal").append(ourTotal);
-    $("#wins").append(wins);
-    $("#losses").append(losses);
-    $("#generatedNumber").append(RandomNumber);
-
-    $("#button-1").on("click", function() {
-        ourTotal = redButton + ourTotal;
+    function startOver() {
+        var RandomNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
+        var redButton = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
+        var blueButton = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
+        var yellowButton = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
+        var greenButton = Math.floor(Math.random() * ((12 - 1) + 1) + 1);
+        var ourTotal = 0;
+        $("#generatedNumber").html(RandomNumber);
         $("#ourTotal").html(ourTotal);
-
-        if (ourTotal === RandomNumber) {
-            wins++;
-            $("#wins").html(wins);
-            console.log(wins);
-        };
-        if (ourTotal > RandomNumber) {
-            losses++;
-            $("#losses").html(losses);
-            console.log(losses);
-        };
-
-
-
-    });
-}
+        $("#button-1").on("click", function () {
+            ourTotal = redButton + ourTotal;
+            $("#ourTotal").html(ourTotal);
+            if (ourTotal === RandomNumber) {
+                wins++;
+                $("#wins").html(wins);
+                startOver();
+            };
+            if (ourTotal > RandomNumber) {
+                losses++;
+                $("#losses").html(losses);
+                startOver();
+            };
+        });
+    }
+    startOver();
 
 
 
