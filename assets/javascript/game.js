c92b80bde3c2cf990158b6f932e51752544478ed
Josh Daniels <joshdaniels@Joshs-iMac.local>
2018-05-29 12:28:21 -0700

    adding if logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,13 @@
+$(document).ready(function() {
+
 // Creating my variables
 var wins = 0;
 var losses = 0;
 var ourTotal = 0;
-var bluebutton = 0;
-var yellowbutton = 0;
-var greenbuttton = 0;
-var anotherRandomNumber = 0;
+var redButton = 0;
+var blueButton = 0;
+var yellowButton = 0;
+var greenbButton = 0;
 
 
 var RandomNumber = Math.floor(Math.random() * ((120-19)+1) + 19);
@@ -14,8 +16,29 @@ var blueButton = Math.floor(Math.random() * ((12-1)+1) + 1);
 var yellowButton = Math.floor(Math.random() * ((12-1)+1) + 1);
 var greenButton = Math.floor(Math.random() * ((12-1)+1) + 1);
 
-$(document).ready(function() {
+    $("#ourTotal").append(ourTotal);
+    $("#wins").append(wins);
+    $("#losses").append(losses);
     $("#generatedNumber").append(RandomNumber);
+
+    $("#button-1").on("click", function() {
+        ourTotal = redButton + ourTotal;
+        $("#ourTotal").html(ourTotal);
+
+        if (ourTotal === RandomNumber) {
+            wins++;
+            $("#wins").html(wins);
+            console.log(wins);
+        };
+        if (ourTotal > RandomNumber) {
+            losses++;
+            $("#losses").html(losses);
+            console.log(losses);
+        };
+
+
+
+    });
 
 
 
